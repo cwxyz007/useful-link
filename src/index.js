8823272fb57a15c544f51faf1a798839b1d43adf
Fantasy <jie844067636@gmail.com>
2019-05-13 20:49:00 +0800

    feat: Get configs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
+import Axios from 'axios'
+import Yaml from 'js-yaml'
+import NProgress from 'nprogress'
+
+NProgress.configure({
+  showSpinner: false
+})
+
+async function getConfigsData () {
+  const { data } = await Axios.get('./configs.yaml')
+  return Yaml.load(data)
+}
+
 async function start () {
-  console.log('test')
+  NProgress.start()
+  const data = await getConfigsData()
+
+  const $pre = document.createElement('pre')
+  $pre.innerHTML = Yaml.dump(data)
+  document.body.appendChild($pre)
+
+  NProgress.done()
 }
 
 start()
