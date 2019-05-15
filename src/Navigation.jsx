a3c8ae1e5307a44e17c8db8b4159353646fa5488
cwxyz007 <jie844067636@gmail.com>
2019-05-15 11:44:45 +0800

    perf: Use font icon instead of favicon if not have one

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,34 +1,41 @@
 import { h } from 'preact'
 
+function FooterLink ({ url, icon }) {
+  return (
+    <a className="navigation-item__link" title={url} href={url} rel="noopener noreferrer" target="_blank">
+      <i className={icon + ' icon'} />
+    </a>
+  )
+}
+
+function Favicon ({ icon, homeUrl }) {
+  const favicon = icon || 'https://s2.googleusercontent.com/s2/favicons?domain_url=' + homeUrl
+  const isSvg = icon && !icon.startsWith('http')
+
+  return isSvg ? (
+    <i className={`navigation-item__icon ${favicon}`} />
+  ) : (
+    <img src={favicon} className="navigation-item__icon" />
+  )
+}
+
 function NavigationItem ({ item, site }) {
+  // placeholder empty item
   if (item === 0) {
     return <div className="navigation-item" style={{ opacity: 0 }} />
   }
 
-  const icon = item.icon || 'https://s2.googleusercontent.com/s2/favicons?domain_url=' + (item.links || {}).web
-
   const links = Object.keys(item.links || {}).map((type) => {
     const url = item.links[type]
     const faIcon = site.navigation.icons[type]
-    return (
-      <a
-        className="navigation-item__link"
-        title={url}
-        href={url}
-        key={item.title + url}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <i className={faIcon + ' icon'} />
-      </a>
-    )
+    return <FooterLink url={url} key={item.title + url} icon={faIcon} />
   })
 
   return (
     <div className="navigation-item">
       <div className="navigation-item__body">
         <div className="flex-v-center">
-          <img src={icon} className="navigation-item__icon" />
+          <Favicon icon={item.icon} homeUrl={(item.links || {}).web} />
           <span className="navigation-item__title">{item.title}</span>
         </div>
         <p className="navigation-item__description">{item.desc}</p>
