0d73d5cc1c78281ab32ab8f58858c41adb399064
cwxyz007 <jie844067636@gmail.com>
2019-05-16 14:02:04 +0800

    perf: Change config file type (yaml -> json)

--- src/configs.js
+++ src/configs.js
@@ -1,12 +1,24 @@
 import Axios from 'axios'
-import Yaml from 'js-yaml'
+import stripJsonComments from 'strip-json-comments'
+
+/**
+ *
+ * @param {string} data
+ */
+function parseJsonWithComment (data) {
+  if (typeof data !== 'string') {
+    return data
+  }
+
+  return JSON.parse(stripJsonComments(data))
+}
 
 export async function getSiteConfigs () {
-  const { data } = await Axios.get('./configs/site.yaml')
-  return Yaml.load(data)
+  const { data } = await Axios.get('./configs/site.json')
+  return parseJsonWithComment(data)
 }
 
 export async function getNavigationData () {
-  const { data } = await Axios.get('./configs/navigation.yaml')
-  return Yaml.load(data)
+  const { data } = await Axios.get('./configs/navigation.json')
+  return parseJsonWithComment(data)
 }
